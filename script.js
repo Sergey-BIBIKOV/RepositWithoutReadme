127ab9f653ab7198d7c0e86abc9ac60adac0915d
Sergey Bibikov <serjantb1@gmail.com>
2022-05-31 17:53:35 +0300

    Исправлени ошибки

--- script.js
+++ script.js
@@ -10,17 +10,18 @@ let servicePrice2 = parseInt(prompt('Сколько это будет стоит
 let FullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(FullPrice - FullPrice * (rollback / 100));
 console.log(servicePercentPrice);
+
 switch (true) {
-    case (fullPrice >= 30000):
+    case (FullPrice >= 30000):
         console.log("Даем скидку в 10 %")
         break;
-    case (fullPrice >= 15000 && fullPrice < 30000):
+    case (FullPrice >= 15000 && fullPrice < 30000):
         console.log("Даем скидку в 5 %")
         break;
-    case (fullPrice > 0 && fullPrice < 15000):
+    case (FullPrice > 0 && fullPrice < 15000):
         console.log("Скидка не предусмотрена")
         break;
-    case (fullPrice <= 0):
+    case (FullPrice <= 0):
         console.log("Что то пошло не так")
         break;
     default:
