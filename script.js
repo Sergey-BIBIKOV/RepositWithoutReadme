212f42f4c62c758bd7aa25c16cd2bc07ffdea426
Sergey Bibikov <serjantb1@gmail.com>
2022-05-31 16:50:50 +0300

    Внес данные в title , screens,screenPrice,adaptive

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let title = 'Lesson02';
-let screens = 'Простые, Cложные, Интерактивные';
-let screenPrice = 50000;
+let title = prompt('Как называется ваш проект');
+let screens = prompt('Простые, Cложные, Интерактивные');
+let screenPrice = prompt('Сколько будет стоить данная работа');
 let rollback = 30;
 let FullPrice = 300000;
-let adaptive = true;
+let adaptive = prompt('нужен ли адаптив на сайте');
 
-console.log(typeof title);
+console.log(title);
 console.log(typeof FullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов' + screenPrice + 'рублей/' + screenPrice / 66.4 + 'долларов/' + screenPrice * 0.44369 + 'гривен/' + screenPrice * 0.099388 + 'юани');
+console.log(Boolean(adaptive));
+console.log(screens);
+console.log(screenPrice + 'рублей/' + screenPrice / 66.4 + 'долларов/' + screenPrice * 0.44369 + 'гривен/' + screenPrice * 0.099388 + 'юани');
 console.log('Стоимость разработки сайта' + FullPrice + 'рублей/' + FullPrice / 66.4 + 'долларов/' + FullPrice * 0.44369 + 'гривен/' + FullPrice * 0.099388 + 'юани');
 
 console.log(screens.toLowerCase().split());
 
 console.log('Процент отката посреднику за работу ' + FullPrice * (rollback / 100));
 
-console.log(+prompt('Как называется ваш проект'))
